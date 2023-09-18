a439551a0d60f002ab50173794526429f5482272
Avirup Sett <settavirup@gmail.com>
2023-09-18 22:05:37 +0530

    Prod build

--- dist/assets/ReactToastify-442f5f0c.js
+++ dist/assets/ReactToastify-442f5f0c.js
@@ -1,4 +1,4 @@
-import{ag as Z,ah as $}from"./vendor-354e7dac.js";import{j as X}from"./jspdf.es.min-a0d00155.js";var V={exports:{}};const q=Z(X);/*!
+import{ay as Z,az as $}from"./vendor-896b1759.js";import{j as X}from"./jspdf.es.min-143be31d.js";var V={exports:{}};const q=Z(X);/*!
  * 
  *               jsPDF AutoTable plugin v3.5.31
  *
